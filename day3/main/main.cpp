2db3dbb094638c23cae439a25ef1f359715456de
Lukas Renner <renner.lukas@gmx.de>
2022-12-04 08:56:45 +0100

    Finished part two of day 3

--- day3/main/main.cpp
+++ day3/main/main.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <set>
 #include <algorithm>
+#include <array>
 
 const std::string filepath {"/home/lukas/prog/AdventOfCode22/day3/main/input"};
 
@@ -12,21 +13,28 @@ int main(){
   std::ifstream fs{filepath};
   std::string line{};
   int res{0};
+  int count{1};
+  std::array<std::set<char>, 3> lines;
 
   while(fs >> line) {
-    std::set<char> left(std::begin(line), std::next(line.begin(), line.size()/2));
-    std::set<char> right(std::next(line.begin(), line.size()/2), std::end(line));
-    std::set<char> diff;
-    std::set_intersection(left.begin(), left.end(), right.begin(), right.end(), std::inserter(diff, diff.begin()));
-    char c{*diff.begin()};
-    int val;
-    if((c < 123) && (c > 96))
-      val = c - 96;
-    else
-      val = c - 64 + 26;
+  
+    lines[(count-1)%3] = std::set<char>(std::begin(line), std::end(line));
+    if(count % 3 == 0) {
+      std::set<char> diff, diff2;
+      std::set_intersection(lines[0].begin(), lines[0].end(), lines[1].begin(), lines[1].end(), std::inserter(diff, diff.begin()));
+      std::set_intersection(lines[2].begin(), lines[2].end(), diff.begin(), diff.end(), std::inserter(diff2, diff2.begin()));
+      char c{*diff2.begin()};
+      int val;
+      if((c < 123) && (c > 96))
+        val = c - 96;
+      else
+        val = c - 64 + 26;
+      
+      std::cout << c << ": " << val << std::endl;
+      res += val; 
+    }
 
-    // std::cout << c << ": " << val << std::endl;
-    res += val;  
+    count++; 
   }
   
   std::cout << "Result: "  << res << std::endl;
